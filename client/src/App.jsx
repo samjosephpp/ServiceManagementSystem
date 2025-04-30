9d36118be6ab7c7ebf3c879f44400ca6e9d2d950
Sam <samjoseph_p@hotmail.com>
2025-04-30 23:21:57 +0300

    Fix error in client/app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,14 @@ import { router } from './routes/router'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { AuthContext, AuthProvider } from './context/authContext'
+import { AuthContext, AuthProvider } from './context/AuthContext'
 
 function App() {
 
-  return (
-    
+  return (    
     <AuthProvider>
       <RouterProvider router={router}>  </RouterProvider>
     </AuthProvider>
-
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const { dashboardRouter } = require('./routes/dashboardRouter');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['https://service-management-system-iota.vercel.app',
+            'http://localhost:5173'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'] 
 }));
