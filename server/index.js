bf352be1911983afb4039cda162558e6be89a184
Sam <samjoseph_p@hotmail.com>
2025-05-01 22:57:06 +0300

    Added frontend domain url to server cors

--- server/index.js
+++ server/index.js
@@ -15,9 +15,7 @@ const { dashboardRouter } = require('./routes/dashboardRouter');
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:['https://service-management-system-iota.vercel.app',
-            'https://service-management-system-bapb5y1g3.vercel.app',
-            'https://service-management-system-sam-josephs-projects-3ce1597e.vercel.app/',
+    origin:['https://servicesystem.vercel.app',
             'http://localhost:5173'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'] 
